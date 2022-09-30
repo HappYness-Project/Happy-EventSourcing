49d78db80a727e9d1fe686a947d2f2e155014af1
kevin <hyunbin7303@gmail.com>
2022-09-30 12:29:18 -0400

    ActivateTodoCommand / DeactivateTodoCommand Implement

---
+++ EventSourcing/HP.Application/Commands/ActivateTodoCommand.cs
@@ -0,0 +1,25 @@
+﻿using HP.Domain;
+using MediatR;
+namespace HP.Application.Commands
+{
+    public record ActivateTodoCommand(string TodoId) : CommandBase<Unit>;
+    public class ActivateTodoCommandHandler : IRequestHandler<ActivateTodoCommand,Unit>
+    {
+        private readonly ITodoRepository _repository;
+        public ActivateTodoCommandHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(ActivateTodoCommand cmd, CancellationToken cancellationToken)
+        {
+            var todo =  await _repository.GetByIdAsync(cmd.TodoId);
+            if(todo == null)
+                throw new ApplicationException($"There is no Todo ID: {cmd.TodoId}.");
+            
+            todo.ActivateTodo(todo.Id);
+            await _repository.UpdateAsync(todo);
+            return Unit.Value;
+        }
+    }
+}
---
+++ EventSourcing/HP.Application/Commands/DeactivateTodoCommand.cs
@@ -0,0 +1,25 @@
+﻿using HP.Domain;
+using MediatR;
+namespace HP.Application.Commands
+{
+    public record DeavtivateTodoCommand(string TodoId) : CommandBase<Unit>;
+    public class DeavtivateTodoCommandHandler : IRequestHandler<DeavtivateTodoCommand,Unit>
+    {
+        private readonly ITodoRepository _repository;
+        public DeavtivateTodoCommandHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(DeavtivateTodoCommand cmd, CancellationToken cancellationToken)
+        {
+            var todo =  await _repository.GetByIdAsync(cmd.TodoId);
+            if(todo == null)
+                throw new ApplicationException($"There is no Todo ID: {cmd.TodoId}.");
+            
+            todo.DeactivateTodo(todo.Id);
+            await _repository.UpdateAsync(todo);
+            return Unit.Value;
+        }
+    }
+}
