f9492727697dfe423a81f73086d279c4ca418722
kevin <hyunbin7303@gmail.com>
2022-10-02 22:03:46 -0400

    Blazor UI | Display Person list #32
    Updating Index.Razor.

--- EventSourcing/BlazorUI/Program.cs
+++ EventSourcing/BlazorUI/Program.cs
@@ -1,6 +1,7 @@
 using BlazorUI.Data;
 using HP.Application;
 using HP.Domain;
+using HP.Infrastructure;
 using HP.Infrastructure.DbAccess;
 using HP.Infrastructure.Repository;
 using MediatR;
@@ -12,13 +13,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddSingleton<IMongoDbContext, MongoDbContext>();
 builder.Services.AddSingleton<IEventStore, EventStoreRepository>();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddTransient<IPersonRepository, PersonRepository>();
 builder.Services.AddTransient<ITodoRepository, TodoRepository>();
-
+builder.Services.AddScoped<IInMemoryBus, InMemoryBus>();
 builder.Services.AddMediatR(typeof(DemoLibMediatREntryPoint).Assembly);
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, true).AddEnvironmentVariables();
 
