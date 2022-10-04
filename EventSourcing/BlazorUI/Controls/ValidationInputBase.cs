25d80e7a2a8af01697d4b2b1a86282c34e5629ed
kevin <hyunbin7303@gmail.com>
2022-10-04 08:23:12 -0400

    Event Call back update / ValidationInput

---
+++ EventSourcing/BlazorUI/Controls/ValidationInputBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorUI.Controls
+{
+    public class ValidationInputBase : ComponentBase
+    {
+        [Parameter]
+        public EventCallback<string> ValueChangedCallBack { get; set; }
+
+        [Parameter]
+        public string Value { get; set; }
+        protected async void HandleInputChanged(ChangeEventArgs eventArgs)
+        {
+            await ValueChangedCallBack.InvokeAsync(eventArgs.Value.ToString());
+        }
+
+    }
+}
