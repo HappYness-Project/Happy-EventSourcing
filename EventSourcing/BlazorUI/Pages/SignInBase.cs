4e993e5abdbcd3f39bb723d2f16c8feef0f2784f
kevin <hyunbin7303@gmail.com>
2022-10-05 08:54:00 -0400

    User validation after adding signin Razor page.

---
+++ EventSourcing/BlazorUI/Pages/SignInBase.cs
@@ -0,0 +1,21 @@
+﻿using HP.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorUI.Pages
+{
+    public class SignInBase : ComponentBase
+    {
+        protected string Day { get; set; } = DateTime.Now.DayOfWeek.ToString();
+        protected string Username { get; set; } = "Kevin Park";
+        protected User User { get; set; } = new User();
+
+        protected void HandleUserNameChanged(ChangeEventArgs eventArgs)
+        {
+            Username = eventArgs.Value.ToString();
+        }
+        protected void HandleUserNameValueChanged(string value)
+        {
+            Username = value;
+        }
+    }
+}
---
+++ EventSourcing/HP.Shared/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HP.Shared
+{
+    // GOing to be used for the Identity service in the future.
+    public class User
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required(ErrorMessage ="The password is required.")]
+        public string Password { get; set; }
+
+    }
+}
