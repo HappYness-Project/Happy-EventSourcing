6408d450f98c9e9aae6c98df24d77adb1a02a947
kevin <hyunbin7303@gmail.com>
2022-11-04 00:47:06 -0400

    Update TodoState.cs

--- EventSourcing/BlazorUI/TodoState.cs
+++ EventSourcing/BlazorUI/TodoState.cs
@@ -6,27 +6,22 @@ namespace BlazorUI
     public class TodoState
     {
         public bool ShowingConfigureDialog { get; private set; }
-        public TodoItem ConfiguringTodoItem { get; private set; }
-        public TodoDetailsDto Todo { get; private set; } = new TodoDetailsDto();
-        public void ShowConfigureTodoItemDialog(TodoItem todoItem)
+        public TodoDetailsDto ConfiguringTodo { get; private set; }
+        public void ShowConfigureTodoItemDialog()
         {
-            ConfiguringTodoItem = todoItem;
             ShowingConfigureDialog = true;
         }
         public void CancelConfigureTodoItemDialog()
         {
-            ConfiguringTodoItem = null;
             ShowingConfigureDialog = false;
         }
         public void ConfirmConfigureTodoItemDialog()
         {
-            Todo.SubTodos.Add(ConfiguringTodoItem);
-            ConfiguringTodoItem = null;
             ShowingConfigureDialog = false;
         }
         public void RemoveConfiguredTodoItem(TodoItem todoItem)
         {
-            Todo.SubTodos.Remove(todoItem);
+            ConfiguringTodo.SubTodos.Remove(todoItem);
         }
     }
 }
