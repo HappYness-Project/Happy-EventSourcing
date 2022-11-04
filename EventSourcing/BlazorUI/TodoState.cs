939e08733788f1c5d25e962daae6e50578d23462
kevin <hyunbin7303@gmail.com>
2022-11-03 22:01:03 -0400

    Adding TodoState for managing Todo object from the client side.

---
+++ EventSourcing/BlazorUI/TodoState.cs
@@ -0,0 +1,32 @@
+﻿using HP.Application.DTOs;
+using HP.Domain;
+
+namespace BlazorUI
+{
+    public class TodoState
+    {
+        public bool ShowingConfigureDialog { get; private set; }
+        public TodoItem ConfiguringTodoItem { get; private set; }
+        public TodoDetailsDto Todo { get; private set; } = new TodoDetailsDto();
+        public void ShowConfigureTodoItemDialog(TodoItem todoItem)
+        {
+            ConfiguringTodoItem = todoItem;
+            ShowingConfigureDialog = true;
+        }
+        public void CancelConfigureTodoItemDialog()
+        {
+            ConfiguringTodoItem = null;
+            ShowingConfigureDialog = false;
+        }
+        public void ConfirmConfigureTodoItemDialog()
+        {
+            Todo.SubTodos.Add(ConfiguringTodoItem);
+            ConfiguringTodoItem = null;
+            ShowingConfigureDialog = false;
+        }
+        public void RemoveConfiguredTodoItem(TodoItem todoItem)
+        {
+            Todo.SubTodos.Remove(todoItem);
+        }
+    }
+}
