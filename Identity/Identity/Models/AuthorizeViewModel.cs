96735f69924e390bb2bc4c3e100c62fc5f14207b
Doosan <doosan@hartmann-controls.com>
2022-11-07 20:41:52 -0500

    initial commit

---
+++ Identity/Identity/Models/AuthorizeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Identity.Models
+{
+    public class AuthorizeViewModel
+    {
+        [Display(Name = "Application")]
+        public string ApplicationName { get; set; }
+
+        [Display(Name = "Scope")]
+        public string Scope { get; set; }
+    }
+}
