c48d8ba6c78ed9c6552bae286c90d4d7f24f0074
kevin <hyunbin7303@gmail.com>
2022-11-08 21:34:36 -0500

    Person query handler for get person user id update / Edit Person form razor page update

---
+++ EventSourcing/HP.Application/Queries/Persons/GetPersonUserId.cs
@@ -0,0 +1,6 @@
+﻿using HP.Application.DTOs;
+using MediatR;
+namespace HP.Application.Queries
+{
+    public record GetPersonUserId(string UserId) : IRequest<PersonDetailsDto>;
+}
--- EventSourcing/HP.Application/Queries/Persons/PersonQueryHandlers.cs
+++ EventSourcing/HP.Application/Queries/Persons/PersonQueryHandlers.cs
@@ -2,13 +2,13 @@
 using HP.Application.DTOs;
 using HP.Domain;
 using MediatR;
-
-
 namespace HP.Application.Queries
 {
     public class PersonQueryHandlers : BaseQueryHandler,
                                         IRequestHandler<GetPersonList, IEnumerable<PersonDetailsDto>>,
-                                        IRequestHandler<GetPersonById, PersonDetailsDto>
+                                        IRequestHandler<GetPersonById, PersonDetailsDto>,
+                                        IRequestHandler<GetPersonByName, PersonDetailsDto>,
+                                        IRequestHandler<GetPersonUserId, PersonDetailsDto>
     {
         private readonly IPersonRepository _personRepository;
         public PersonQueryHandlers(IMapper mapper, IPersonRepository personRepository) : base(mapper)
@@ -26,11 +26,25 @@ namespace HP.Application.Queries
 
         public async Task<PersonDetailsDto> Handle(GetPersonById request, CancellationToken cancellationToken)
         {
-            var check = await _personRepository.GetByIdAsync(request.Id);
+            var check = await _personRepository.GetByIdAsync(request.Id.ToUpperInvariant());
             if (check == null)
                 throw new ApplicationException($"Person not exist. Person ID:{request.Id}");
 
             return _mapper.Map<PersonDetailsDto>(check);
+        }
+
+        public async Task<PersonDetailsDto> Handle(GetPersonByName request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<PersonDetailsDto> Handle(GetPersonUserId request, CancellationToken cancellationToken)
+        {
+            var person = await _personRepository.GetPersonByUserIdAsync(request.UserId.ToUpperInvariant());
+            if (person == null)
+                throw new ApplicationException($"Person not exist. Person ID:{request.UserId}");
+
+            return _mapper.Map<PersonDetailsDto>(person);
         }
     }
 }
