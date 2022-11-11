121f33b9b9f22e53dd6ee8f3180f50b33661c24d
kevin <hyunbin7303@gmail.com>
2022-11-11 12:41:04 -0500

    Model Design | Create Category model for Person domain. #53

---
+++ EventSourcing/HP.Domain/Categories/Category.cs
@@ -0,0 +1,10 @@
+﻿using HP.Domain.Common;
+namespace HP.Domain.Categories
+{
+    public class Category : BaseItem,  IAggregateRoot
+    {
+        public string UserId { get; set; }
+        public IList<CategoryItem> Items { get; set; }
+
+    }
+}
---
+++ EventSourcing/HP.Domain/Categories/CategoryItem.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HP.Domain.Categories
+{
+    public class CategoryItem
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public string Desc { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+}
