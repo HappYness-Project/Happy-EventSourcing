2efef2569d9bdd567bfe0dfb97a4ac8fc8eb8326
kevin <hyunbin7303@gmail.com>
2022-11-14 14:41:16 -0500

    Clicking for Displaying Item Edit Data.

--- EventSourcing/BlazorUI/Components/ItemEdit.razor.cs
+++ EventSourcing/BlazorUI/Components/ItemEdit.razor.cs
@@ -1,10 +1,25 @@
-﻿using HP.Shared;
+﻿using BlazorUI.Services.ItemEdit;
+using HP.Shared;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorUI.Components
 {
     public partial class ItemEdit
     {
+        [Inject] private ItemEditService ItemEditService { get; set; }
         private BaseItem Item { get; set; } = new BaseItem();
         private int TotalNumber { get; set; }
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            ItemEditService.EditItemChanged += HandleEditItemChanged;
+            Item = ItemEditService.EditItem;
+        }
+
+        private void HandleEditItemChanged(object? sender, ItemEditEventArgs e)
+        {
+            Item = e.Item;
+            StateHasChanged();
+        }
     }
 }
--- EventSourcing/BlazorUI/Components/ItemElement.razor.cs
+++ EventSourcing/BlazorUI/Components/ItemElement.razor.cs
@@ -6,18 +6,12 @@ namespace BlazorUI.Components
 {
     public partial class ItemElement<TItem> : ComponentBase where TItem : BaseItem
     {
-        [Parameter]
-        public RenderFragment MainFragment { get; set; }
-        [Parameter]
-        public RenderFragment DetailFragment { get; set; }
-        [Parameter]
-        public TItem Item { get; set; }
-        [CascadingParameter]
-        public string ColorPrefix { get; set; }
-        [CascadingParameter]
-        public int TotalNumber { get; set; }
-        [Inject]
-        private ItemEditService ItemEditService { get; set; }
+        [Parameter] public RenderFragment MainFragment { get; set; }
+        [Parameter] public RenderFragment DetailFragment { get; set; }
+        [Parameter] public TItem Item { get; set; }
+        [CascadingParameter] public string ColorPrefix { get; set; }
+        [CascadingParameter] public int TotalNumber { get; set; }
+        [Inject] private ItemEditService ItemEditService { get; set; }
         private string DetailAreaId { get; set; }
         protected override void OnParametersSet()
         {
