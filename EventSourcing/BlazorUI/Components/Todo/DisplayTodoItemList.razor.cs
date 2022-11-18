847b062710319eef74c9a4d8744dc36b98c5a216
kevin <hyunbin7303@gmail.com>
2022-11-18 18:02:59 -0500

    Adding TodoItemElement Razor component

--- EventSourcing/BlazorUI/Components/Todo/DisplayTodoItemList.razor.cs
+++ EventSourcing/BlazorUI/Components/Todo/DisplayTodoItemList.razor.cs
@@ -7,30 +7,15 @@ using Microsoft.AspNetCore.Components;
 
 namespace BlazorUI.Components.Todo
 {
-    public partial class TodoItemElement : ComponentBase
+    public partial class DisplayTodoItemList : ComponentBase
     {
         [Inject] public IMediator _mediator { get; set; }
-        [Parameter] public TodoItem TodoItem { get; set; }
-        [Parameter] public TodoDetailsDto ParentTodo { get; set; }
-        [Parameter] public EventCallback<bool> OnTodoItemSelection { get; set; }
+        [Parameter] public TodoDetailsDto Todo { get; set; }
         public TodoItem SelectTodoItem { get; set; }
         public bool UpdateTodoItemDialogOpen { get; set; } = false;
         public string newTodoStatus { get; set; } = string.Empty;
         protected bool IsSelected { get; set; }
-
-        protected async Task CheckBoxChanged(ChangeEventArgs e)
-        {
-            IsSelected = (bool)e.Value;
-            await OnTodoItemSelection.InvokeAsync(IsSelected);
-        }
-        private async Task DeleteTodoSubItem(string todoId, string subTodoId)
-        {
-            bool isRemoved = await _mediator.Send(new DeleteTodoItemCommand(todoId, subTodoId));
-            if (isRemoved)
-                await LoadTodoData();
-
-        }
-        private void OpenUpdateTodoItemDialog(TodoItem todoItem)
+        private void OpenUpdateTodoItemDialog(TodoDetailsDto todo, TodoItem todoItem)
         {
             SelectTodoItem = todoItem;
             UpdateTodoItemDialogOpen = true;
@@ -45,11 +30,6 @@ namespace BlazorUI.Components.Todo
         private async Task StatusSelected(ChangeEventArgs args)
         {
             newTodoStatus = args.Value as string;
-        }
-        private async Task LoadTodoData()
-        {
-            ParentTodo = await _mediator.Send(new GetTodoById(ParentTodo.TodoId));
-            StateHasChanged();
         }
     }
 }
