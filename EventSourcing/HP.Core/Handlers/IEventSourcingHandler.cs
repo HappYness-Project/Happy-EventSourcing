3d01eb8030a88820b5fe729e5bd7dfa885323a79
kevin <hyunbin7303@gmail.com>
2022-12-24 14:17:50 -0500

    Infrastructure | Kafka - Consumer Design and setup #25

---
+++ EventSourcing/HP.Core/Handlers/IEventSourcingHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace HP.Core.Handlers
+{
+        public interface IEventSourcingHandler<T>
+    {
+        Task SaveAsync(AggregateRoot aggregate);
+        Task<T> GetByIdAsync(Guid aggregateId);
+    }
+
+}
