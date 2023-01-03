25ec9ea10a48c61a90ac469d794979ecf3173806
kevin <hyunbin7303@gmail.com>
2023-01-03 12:35:12 -0500

    Create EventJsonConverter.cs

---
+++ EventSourcing/HP.Infrastructure/Kafka/EventJsonConverter.cs
@@ -0,0 +1,28 @@
+﻿using HP.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HP.Infrastructure.Kafka
+{
+    public class EventJsonConverter : JsonConverter<IDomainEvent>
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeToConvert.IsAssignableFrom(typeof(IDomainEvent));
+        }
+        public override IDomainEvent? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, IDomainEvent value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
