73dfc5ef7cf27b08c6e1b411b2bb9ce332fea693
kevin <hyunbin7303@gmail.com>
2023-01-04 20:59:24 -0500

    Create BaseCommandHandler for handling multiple interfaces. #75

---
+++ EventSourcing/HP.Application/Commands/BaseCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using HP.Core.Events;
+using HP.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HP.Application.Commands
+{
+    public abstract class BaseCommandHandler
+    {
+        private readonly IEventProducer _eventProducer;
+        protected BaseCommandHandler(IEventProducer eventProducer)
+        {
+            _eventProducer = eventProducer;
+        }
+        protected async Task ProduceDomainEvents(string topic, IReadOnlyCollection<IDomainEvent> domainEvents)
+        {
+            foreach(var domainEvent in domainEvents)
+                await _eventProducer.ProducerAsync(topic, domainEvent);
+        }
+    }
+}
