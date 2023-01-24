0ddb942517dd7281bf50eaf41b79024084389326
kevin <hyunbin7303@gmail.com>
2023-01-23 19:11:35 -0500

    Nuget setup for the HP

---
+++ EventSourcing/HP.Infrastructure/DbAccess/PostgreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HP.Infrastructure.DbAccess
+{
+    public class PostgreDbContext : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+        public PostgreDbContext(IConfiguration configuration)
+        {
+            _configuration= configuration;
+        }
+        protected override void OnCo
+    }
+}
