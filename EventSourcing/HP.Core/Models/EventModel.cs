b6292277c3a4c839c80912413f03d95abb16c724
kevin <hyunbin7303@gmail.com>
2023-02-06 16:51:46 -0500

    Event Model Redesign for the data. #87

---
+++ EventSourcing/HP.Core/Models/EventModel.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+namespace HP.Core.Models
+{
+    public class EventModel
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Guid AggregateIdentifier { get; set; }
+        public string AggregateType { get; set; }
+        public int Version { get; set; }
+        public string EventType { get; set; }
+        public DomainEvent EventData { get; set; }
+    }
+}
