c009e739f8b8e89b273de30dafb0218665aa6334
kevin <hyunbin7303@gmail.com>
2023-02-20 17:18:30 -0500

    Update UpdatePersonCommand.cs

--- EventSourcing/HP.Application/Commands/Persons/UpdatePersonCommand.cs
+++ EventSourcing/HP.Application/Commands/Persons/UpdatePersonCommand.cs
@@ -1,29 +1,26 @@
 ﻿using HP.Core.Commands;
+using HP.Core.Common;
 using HP.Core.Events;
-using HP.Domain.People.Write;
 using MediatR;
 
-namespace HP.Application.Commands.Person
+namespace HP.Application.Commands.Persons
 {
     public record UpdatePersonCommand(Guid PersonId, string? PersonType, string? GoalType, int? GroupId = null) : BaseCommand;
     public class UpdatePersonCommandHandler : BaseCommandHandler, IRequestHandler<UpdatePersonCommand, CommandResult>
     {
-        private readonly IPersonAggregateRepository _repository;
-        public UpdatePersonCommandHandler(IEventProducer eventProducer, IPersonAggregateRepository personRepository) : base(eventProducer)
+        private readonly IAggregateRepository<Domain.Person> _personRepository;
+        public UpdatePersonCommandHandler(IEventProducer eventProducer, IAggregateRepository<Domain.Person> personRepository) : base(eventProducer)
         {
-            _repository = personRepository ?? throw new ArgumentNullException(nameof(personRepository));
+            _personRepository = personRepository ?? throw new ArgumentNullException(nameof(personRepository));
         }
         public async Task<CommandResult> Handle(UpdatePersonCommand request, CancellationToken cancellationToken)
         {
-            var person = _repository.GetByIdAsync(request.PersonId).Result;
+            var person = await _personRepository.GetByAggregateId<Domain.Person>(request.PersonId);
             if (person == null)
                 throw new ApplicationException($"PersonId : {request.PersonId} is not exist.");
 
             person.UpdateBasicInfo(request.PersonType, request.GoalType, request.GroupId);
-            var check = await _repository.UpdatePersonAsync(person);
-            if (check == null)
-                return new CommandResult(false, "Updated failure. ", person.Id.ToString());
-
+            await _personRepository.PersistAsync(person);
             await ProduceDomainEvents(person.UncommittedEvents);
             return new CommandResult(true, "", person.Id.ToString());
         }
