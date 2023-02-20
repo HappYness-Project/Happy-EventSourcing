9253e0453023c3b0d4238178fe993aed2728ee30
kevin <hyunbin7303@gmail.com>
2023-02-20 17:28:06 -0500

    Update UpdatePersonGroupCommand.cs

--- EventSourcing/HP.Application/Commands/Persons/UpdatePersonGroupCommand.cs
+++ EventSourcing/HP.Application/Commands/Persons/UpdatePersonGroupCommand.cs
@@ -1,25 +1,25 @@
 ﻿using HP.Core.Commands;
+using HP.Core.Common;
 using HP.Core.Events;
-using HP.Domain.People.Write;
 using MediatR;
 namespace HP.Application.Commands.Person
 {
     public record UpdatePersonGroupCommand(Guid PersonId, int GroupId) : BaseCommand;
     public class UpdatePersonGroupCommandHandler : BaseCommandHandler, IRequestHandler<UpdatePersonGroupCommand, CommandResult>
     {
-        private readonly IPersonAggregateRepository _repository;
-        public UpdatePersonGroupCommandHandler(IEventProducer eventProducer, IPersonAggregateRepository personRepository) : base(eventProducer)
+        private readonly IAggregateRepository<Domain.Person> _personRepository;
+        public UpdatePersonGroupCommandHandler(IEventProducer eventProducer, IAggregateRepository<Domain.Person> personRepository) : base(eventProducer)
         {
-            _repository = personRepository ?? throw new ArgumentNullException(nameof(personRepository));
+            _personRepository = personRepository ?? throw new ArgumentNullException(nameof(personRepository));
         }
         public async Task<CommandResult> Handle(UpdatePersonGroupCommand request, CancellationToken cancellationToken)
         {
-            var person = await _repository.GetByIdAsync(request.PersonId);
+            var person = await _personRepository.GetByAggregateId<Domain.Person>(request.PersonId);
             if (person == null)
                 throw new ApplicationException($"PersonId : {request.PersonId} is not exist.");
 
             person.UpdateGroupId(request.GroupId);
-            await _repository.UpdateAsync(person);
+            await _personRepository.PersistAsync(person);
             await ProduceDomainEvents(person.UncommittedEvents);
             return new CommandResult(true, "", person.Id.ToString());
         }
