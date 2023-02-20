b8cbd5465ed42f4ed76914a3f94d904b1cd4c034
kevin <hyunbin7303@gmail.com>
2023-02-20 17:01:46 -0500

    Update UpdateTodoCommand.cs

--- EventSourcing/HP.Application/Commands/Todo/UpdateTodoCommand.cs
+++ EventSourcing/HP.Application/Commands/Todo/UpdateTodoCommand.cs
@@ -1,4 +1,5 @@
 ﻿using HP.Core.Commands;
+using HP.Core.Common;
 using HP.Domain.Todos.Write;
 using MediatR;
 
@@ -7,19 +8,19 @@ namespace HP.Application.Commands.Todo
     public record UpdateTodoCommand(Guid TodoId, string Title, string type, string Description, string[] Tags, DateTime? TargetStartDate = null, DateTime? TargetEndDate = null) : BaseCommand;
     public class UpdateTodoCommandHandler : IRequestHandler<UpdateTodoCommand, CommandResult>
     {
-        private readonly ITodoAggregateRepository _repository;
-        public UpdateTodoCommandHandler(ITodoAggregateRepository todoRepository)
+        private readonly IAggregateRepository<Domain.Todo> _todoRepository;
+        public UpdateTodoCommandHandler(IAggregateRepository<Domain.Todo> todoRepository)
         {
-            _repository = todoRepository;
+            _todoRepository = todoRepository ?? throw new ArgumentNullException(nameof(todoRepository));
         }
         public async Task<CommandResult> Handle(UpdateTodoCommand request, CancellationToken cancellationToken)
         {
-            var todo = await _repository.GetByIdAsync(request.TodoId);
+            var todo = await _todoRepository.GetByAggregateId<Domain.Todo>(request.TodoId);
             if (todo == null)
                 throw new ApplicationException($"TodoId:{request.TodoId}, does not exist.");
 
             todo.Update(request.Title, request.type, request.Description, request.Tags, request.TargetStartDate, request.TargetEndDate);
-            await _repository.UpdateAsync(todo);
+            await _todoRepository.PersistAsync(todo);
             return new CommandResult(true, "Todo information has been changed", todo.Id.ToString());
         }
     }
