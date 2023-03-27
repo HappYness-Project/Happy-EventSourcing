d885c83596875f9c5dac9e4b1aaf6f988d77d58a
kevin <hyunbin7303@gmail.com>
2023-03-27 15:30:53 -0400

    Checking App settings for dockercompose

--- src/EventSourcing/HP.Infrastructure/Kafka/ConsumerHostedService.cs
+++ src/EventSourcing/HP.Infrastructure/Kafka/ConsumerHostedService.cs
@@ -3,13 +3,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace HP.Infrastructure.Kafka
 {
     public class ConsumerHostedService : IHostedService
